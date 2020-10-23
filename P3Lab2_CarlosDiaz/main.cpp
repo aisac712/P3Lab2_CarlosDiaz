c6e8f92623281258bdb998addc16fbad1fa8729d
aisac <isaac.diaz712@gmail.com>
2020-10-23 16:02:27 -0600

    Ejercicio 2 resuleto

--- P3Lab2_CarlosDiaz/main.cpp
+++ P3Lab2_CarlosDiaz/main.cpp
@@ -8,6 +8,7 @@ using std::endl;
 
 void factorizar(double n){
     int primos[25] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97};
+    int primosUsados[25] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0};
     
     int temp = 1, i = 0;
     double disminuye=n;
@@ -24,6 +25,7 @@ void factorizar(double n){
             cout << p << endl;
             
             if(p == (int)p ){
+                primosUsados[i] = primosUsados[i]+1;
                 disminuye = disminuye / primos[i];
                 temp = temp * primos[i];
                 salida += std::to_string(primos[i]) + "*";
@@ -39,18 +41,62 @@ void factorizar(double n){
         
         //seguirConNumero = true;
     }
+    salida += "1";
     
     cout << salida << endl;
     
+    string salida2 = "";
+    for(int i=0; i<25; i++){
+        if(primosUsados[i]!=0){
+            salida2 += std::to_string(primos[i]) + "^" + std::to_string(primosUsados[i]) + " * ";
+        }
+    }
+    salida2+="1";
+    cout << salida2 << endl;
+    
 }
+
+//SEGUNDO EJERCICIO
+double exponencial(double x, double n){
+    //double r=0;                                      //resultado
+    if(n==0){
+        return 1;
+    } else {
+        return x * exponencial(x, n-1);
+    }
+}
+
+double factorial(double n){
+    //double r=0;                                      //resultado
+    if(n==0 || n==1){
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+void funcionExponencial(double lim, double x){
+    double sum = 0;
+    cout << lim << " " << x << endl;
+    
+    for(int n=0; n<=lim; n++){
+        double num = exponencial(x, n);
+        double denom = factorial(n);
+        //cout << num << "/" << denom;
+        sum += num/denom;
+    }
+    cout << "Resultado: " << sum << endl;
+}
+
+//MAIN
 int main(int argc, char** argv) {
     
     
     int op;
-    while(op!=3){
+    while(op!=4){
         cout<<"          MENU"<<endl
         <<" 1. Teorema Fundamental de la Aritmética"<<endl
-        <<" 2. Aproximar valor de pi con n"<<endl
+        <<" 2. Aproximar valor de euler a la x"<<endl
         <<" 3. Salir"<<endl
         <<" 4. Salir"<<endl
         <<"Ingrese una opción: ";
@@ -73,11 +119,11 @@ int main(int argc, char** argv) {
                 
                 break;
             } case 2: {
-                double lim;
-                cout << "Ingresa lim para hacer el cálculo: ";
-                cin  >> lim;
+                double x;
+                cout << "Ingresa x para hacer el cálculo: ";
+                cin  >> x;
                 
-                
+                funcionExponencial(30, x);
                 break;
             } case 3: {
                 break;
