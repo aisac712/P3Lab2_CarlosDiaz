a48d8fd954cdbedf0a525cfd51aa15e44f9804e2
aisac <isaac.diaz712@gmail.com>
2020-10-23 16:30:38 -0600

    Ejercicio 3

--- P3Lab2_CarlosDiaz/main.cpp
+++ P3Lab2_CarlosDiaz/main.cpp
@@ -6,14 +6,15 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//PRIMER EJERCICIO
 void factorizar(double n){
     int primos[25] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97};
     int primosUsados[25] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0};
     
-    int temp = 1, i = 0;
+    int temp = 1, i = 0;                                            //temp es la multiplicación de todos los números primos que forman a n
     double disminuye=n;
     string salida = "";
-    while(temp/n!=1){
+    while(temp/n!=1){                                               //mientras sean distintos es lo mismo que decir mientras su cociente sea dif a 1
         bool seguirConNumero = true;
         
         
@@ -21,19 +22,19 @@ void factorizar(double n){
             double p = disminuye/primos[i];
             
             //cout << primos[i] << endl;
-            cout << "p=" << disminuye << "/" << primos[i];
-            cout << p << endl;
+            //cout << "p=" << disminuye << "/" << primos[i];
+            //cout << p << endl;
             
-            if(p == (int)p ){
+            if(p == (int)p ){                                       //si el número es entero, se sigue dividiendo con ese mismo prmo
                 primosUsados[i] = primosUsados[i]+1;
                 disminuye = disminuye / primos[i];
                 temp = temp * primos[i];
                 salida += std::to_string(primos[i]) + "*";
                 //seguirConNumero = true;
                 //cout << "el número es entero";
-            } else{
+            } else{                                                 //si el número resulta decimal, entonces no es con ese primo y...
                 //cout << "el número NO es entero";
-                i++;
+                i++;                                                //...se procede a calcularlo con el número siguiente
                 seguirConNumero = false;
             }
         }
@@ -88,6 +89,40 @@ void funcionExponencial(double lim, double x){
     cout << "Resultado: " << sum << endl;
 }
 
+//TEERCER EJERCICIO
+void triangular(int n){
+    for (int i=1; i<=n; i++){
+        int fila[i];
+        
+        //para rellenar de prueba
+        /*if(i==1 || i==2){
+            for (int j=0; j<i; j++){
+                fila[j] = 1;
+            }
+        } else {
+            for (int j=0; j<i; j++){
+                if(j==1 || j==(i-1)){
+                    fila[j] = 1;
+                }
+                if(j==2 || j==(i-2)){
+                    fila[j] = i+1;
+                }
+            }
+        }*/
+        //para rellenar
+        for (int j=0; j<i; j++){
+            fila[j] = 1;
+        }
+        
+        //para imprimir
+        for (int j=0; j<i; j++){
+            cout << fila[j] << " ";
+        }
+        cout << "" << endl;
+    }
+}
+
+
 //MAIN
 int main(int argc, char** argv) {
     
@@ -97,7 +132,7 @@ int main(int argc, char** argv) {
         cout<<"          MENU"<<endl
         <<" 1. Teorema Fundamental de la Aritmética"<<endl
         <<" 2. Aproximar valor de euler a la x"<<endl
-        <<" 3. Salir"<<endl
+        <<" 3. Triángulo de Pascal"<<endl
         <<" 4. Salir"<<endl
         <<"Ingrese una opción: ";
 
@@ -126,6 +161,11 @@ int main(int argc, char** argv) {
                 funcionExponencial(30, x);
                 break;
             } case 3: {
+                int n;
+                cout << "Ingresa la cantidad de filas n: ";
+                cin  >> n;
+                
+                triangular(n);
                 break;
             } case 4 : {
                 cout << "Nos vemos" << endl;
